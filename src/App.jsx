de1e3a7a8707a5e683c6a191d211fa13254a9919
yerlin <yeyeaqe@gmail.com>
2024-11-06 22:09:51 -0500

    render App y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
 import Form from './Components/Form'
 
 function App() {
+  const [infoEstudiante, setinfoEstudiante] = useState(null);
+  const [error, setError] = useState('');
   
+  const handleFormSubmit = (data) => {
+    if (validacion(data)) {
+      setinfoEstudiante(data);
+      setError('');
+    } else {
+      setError('Por favor chequea que la información sea correcta');
+    }
+  };
+  const validacion = ({ nombre, apellido }) => {
+    return (
+      nombre.length >= 3 && !nombre.startsWith(' ') &&
+      apellido.length >= 6
+    );
+  };
   return (
-    <div className='App'>
-      <h1>Carga de estudiantes</h1>
-      <Form></Form>
-      <Card/>
+    <div className="App">
+      <h1>Formulario de Estudiante</h1>
+      <Form onSubmit={handleFormSubmit} />
+      {error && <p className="error">{error}</p>}
+      {infoEstudiante && <Card {...infoEstudiante} />}
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,11 @@
 const Card = ({ nombre, apellido, curso }) => {
   return (
-    <div className="Card">
+    <>
         <h3>Información del Estudiante:</h3>
         <p>Nombre: {nombre}</p>
         <p>Apellido: {apellido}</p>
         <p>Curso: {curso}</p>
-    </div>
+    </>
   )
 }
 
